e69ecb503f591692ac8739e1637182bdc2256e94
i.nedzvetskiy <stels-cs@ya.ru>
2015-04-18 16:00:14 +0300

    Google analystic

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -75,20 +75,12 @@ if (!function_exists("isActive")) {
                 <br>
             </div>
         </div>
-        <div class="container">
-            <div class="row">
-                <?php 
-                    // echo $this->render('../global/category-menu');
-                ?>
-            </div>
-        </div>
 	   <?= $content ?>
 	</div>
 
     <footer class="footer">
         <div class="container">
             <p class="pull-left">&copy; Доставка еды из Макдональдс Старфудс в Архангельске <?php echo \app\assets\ConfigHelper::getPartnerEmail();?> - контакт для партнеров</p>
-            
         </div>
     </footer>
 
@@ -99,9 +91,7 @@ if (!function_exists("isActive")) {
     var ADD_DELIVERY_PRICE = '<?php echo Yii::$app->params['ADD_DELIVERY_PRICE']?>';
 </script>
 <script src="<?php echo \Yii::$app->urlManager->createUrl(['/js/chart.js']); ?>"></script>
-<?php if($_SERVER['HTTP_HOST'] != "dostavka29.loc"):?>
-<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter29055700 = new Ya.Metrika({id:29055700, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/29055700" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
-<?php endif;?>
+<?php echo $this->render('counters'); ?>
 </body>
 </html>
 <?php $this->endPage() ?>
