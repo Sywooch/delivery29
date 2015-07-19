a6d9f9523fad0b1e356e03e46311059016354e4b
stels-cs <stels-cs@ya.ru>
2015-07-19 23:22:54 +0300

    Disable delivery controls

--- views/admin/panel.php
+++ views/admin/panel.php
@@ -10,13 +10,26 @@
 	<p>
 		Почта для отправки оповещеий <input type="email" id="email" value="<?php echo $data['newOrderNoticeEmail']?>"> <button onclick="saveParam('newOrderNoticeEmail', '#email', 'Email сохранен')" class='btn btn-sm'>Сохранить</button>
 	</p>
+
+    <p>
+        <label>Доставка работает <input id="DISABLE_DELIVERY" type="checkbox" <?php if($data['DISABLE_DELIVERY'] != 'Y') echo "checked" ?> onchange="saveParam('DISABLE_DELIVERY', '#DISABLE_DELIVERY', 'Статус сохранен', this.checked ? 'N' : 'Y')" value="Y"></label>
+    </p>
+    <p>
+        Причина простоя (необязательно) <input type="text" id="DISABLE_DELIVERY_REASON" value="<?php echo $data['DISABLE_DELIVERY_REASON']?>"> <button onclick="saveParam('DISABLE_DELIVERY_REASON', '#DISABLE_DELIVERY_REASON', 'текст сохранен')" class='btn btn-sm'>Сохранить</button>
+    </p>
+
 </div>
 
 <script>
 	
-	function saveParam(pName, valId, success)
+	function saveParam(pName, valId, success, value)
 	{
-		var val = $(valId).val();
+        if (typeof value == "undefined")
+        {
+            var val = $(valId).val();
+        } else {
+            val = value;
+        }
 		$.get('/params/create', {'name':pName,'value':val}).success( function () {alert(success);} );
 	}
 
